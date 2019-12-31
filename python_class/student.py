dbc6b9d4a67cd27f09df0fbcc66e5bb4de0d519e
mosangzi <zhangxiaokaihit@gmail.com>
2020-01-01 01:12:08 +0800

    新增对类的调用

--- python_class/student.py
+++ python_class/student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 
+
 class Student(object):
     def __init__(self, name, age):
         self.name = name
@@ -15,3 +16,15 @@ class Student(object):
         else:
             print(f'{self.name}可以看18+电影')
 
+
+def run():
+    stu1 = Student('John', 35)
+    stu1.study('算法导论')
+    stu1.watch_movie()
+    stu2 = Student('王大锤', 17)
+    stu2.study('立体几何')
+    stu2.watch_movie()
+
+
+if __name__ == '__main__':
+    run()
